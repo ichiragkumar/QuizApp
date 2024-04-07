de417a437e047f934fceb4d9095ba02bd5992e0f
ichiragkumar <chiragroy3030@gmail.com>
2024-04-08 03:41:10 +0530

    Done

--- app/src/main/java/com/example/quizapp/ScoreScreen.kt
+++ app/src/main/java/com/example/quizapp/ScoreScreen.kt
@@ -4,12 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.firebase.auth.FirebaseAuth
 
 class ScoreScreen : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -20,10 +25,26 @@ class ScoreScreen : AppCompatActivity() {
             insets
         }
 
+        auth = FirebaseAuth.getInstance()
+        val emailTextView: TextView = findViewById(R.id.emailTextView)
+
+        // Check if the user is logged in
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            // User is logged in, retrieve and display the email address
+            val email = currentUser.email
+            emailTextView.text = "$email"
+        } else {
+            // User is not logged in, display a message
+            emailTextView.text = "Email: Not logged in"
+        }
+
         val score = intent.getIntExtra("SCORE", 0)
+        Toast.makeText(this, "Your Score: $score", Toast.LENGTH_SHORT).show()
+
 
         val scoreTextView: TextView = findViewById(R.id.scoreTextView)
-        scoreTextView.text = "Your Score: $score"
+        scoreTextView.text = "$score"
 
         // go to QuizsScreen
         val gotoQuizessScreen = findViewById<Button>(R.id.button)
